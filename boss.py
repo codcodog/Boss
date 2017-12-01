285cf15b150e0050a7ee29a9741fe409fec3ad39
codcodog <codcodog@gmail.com>
2017-12-01 18:48:49 +0800

    合并操作

--- boss.py
+++ boss.py
@@ -1,5 +1,12 @@
 from proxy import proxy
+from crawl import spider
 
 if __name__ == '__main__':
+    # 获取代理ip
     proxy = proxy.Proxy()
-    proxy.run()
+    proxy = proxy.run()
+
+    # 开始爬取
+    key_word = 'php'
+    spider = spider.Spider(key_word, proxy)
+    spider.run()
